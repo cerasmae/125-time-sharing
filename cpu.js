1d86469205adf89263d9d9161227dec7a17b9916
Mae Celine Erasmo <mae.celinerasmo@gmail.com>
2017-09-08 19:36:18 +0800

    allowed users to use more than one resource

--- cpu.js
+++ cpu.js
@@ -54,41 +54,44 @@ function User(number, length, color){
 function drawResources(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	var height = canvas.height/resources.length;
-	var width = 175;
+	var width = 200;
 	var startX = 15;
 	var startY = 0;
 	var queueX = startX;
+	var queueTotal = 0;
 
 	for(var i = 0; i < resources.length; i++){
 		ctx.fillStyle = resources[i].color;
 		ctx.fillRect(startX, startY+5, width, height-10);
 		queueX = startX + width + 15;
+		queueTotal = 0;
 
 		ctx.fillStyle = "black";
 		ctx.fillText(resources[i].name, startX+(width/10), startY+(height/2) );
-
 		if(resources[i].queue.length > 0){
 			ctx.fillStyle = resources[i].queue[0].color;
 			ctx.fillRect(queueX+5, startY+5, width-10, height-10);
 			ctx.fillStyle = "black";
 			ctx.fillText(resources[i].queue[0].name+" ("+ resources[i].queue[0].length +")", queueX+(width/10), startY+(height/2) );
 			queueX+=width;
+			queueTotal = resources[i].queue[0].length;
 			for(var j = 1; j < resources[i].queue.length; j++){
 				ctx.fillStyle = resources[i].queue[j].color;
 				ctx.fillRect(queueX+5, startY+5, width-10, height-10);
 				ctx.fillStyle = "black";
 				ctx.fillText(resources[i].queue[j].name, queueX+(width/10), startY+(height/2) );
-				ctx.fillText("(waiting)", queueX+(width/4.5), startY+(height*0.80) );
+				ctx.fillText("(waiting until "+queueTotal+")", queueX+(width/12), startY+(height*0.80) );
+				queueTotal += resources[i].queue[j].length;
 				queueX+=width;
 			}
+			ctx.fillText("Free at: "+queueTotal, startX+(width/4.5), startY+(height*0.80) );
 		} else {
 			ctx.fillStyle = "black";
-			ctx.fillText("Free", queueX+(width/10), startY+(height/2) );
+			ctx.fillText("Free", startX+(width/3), startY+(height*0.80) );
 		}
 		startY+=height;
 	}
 }
-
 
 function checkResources(){
 	for(var i = 0; i < resources.length; i++){
@@ -106,14 +109,24 @@ for(var i = 1; i <= rLimit; i++){
 	resources.push(new Resource(i, color=randomColor({luminosity: 'light'})));
 }
 
-var uLimit = 30
+var uLimit = generateRandom(30);
 
 console.log("users: "+uLimit);
 var newUser = {};
 for(var i = 1; i <= uLimit; i++){
-	var resource = generateRandom(rLimit);
-	newUser = new User(i, length=generateRandom(30), color=randomColor({luminosity: 'light'}));
-	resources[resource-1].addQueue(newUser);
+	var resourceNum = generateRandom(rLimit);
+	for(var j = 0; j < resourceNum; j++ ){
+		var resource = generateRandom(rLimit);
+		newUser = new User(i, length=generateRandom(30), color=randomColor({luminosity: 'light'}));
+
+		if( resources[resource-1].queue.length > 0 ){
+			if( newUser.name != resources[resource-1].queue[resources[resource-1].queue.length-1].name ){
+				resources[resource-1].addQueue(newUser);
+			}
+		} else{
+			resources[resource-1].addQueue(newUser);
+		}
+	}
 }
 
 drawResources(rLimit);
